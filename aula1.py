21fd030fbc1170eb0fc3fe308d50c5ef25e0f0ae
Chris <cristovao.santos@etec.sp.gov.br>
2025-08-20 12:57:36 -0300

    Prints for python

---
+++ aula1.py
@@ -0,0 +1,15 @@
+# Definindo variáveis
+nome = "Cristovao SMS"
+idade = 18
+altura = 1.80
+situacaoTrabalho = False
+
+
+
+# Exibindo os dados com função print
+
+# print("A soma é: ",2+3)
+# print("A Multiplicação é: {}".format(2*3))
+# print(f"A Divisão é: {10/2}")   
+
+print(f"Seu nome é {nome}, sua idade é {idade} anos, sua altura {altura}M, sua situação de trabalho é {"Empregado" if situacaoTrabalho else "Desempregado"}")
